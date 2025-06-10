17baa5a45b7489a7483fb177e40a5f5bdd19a883
tominien <tom.leber.toulouse@gmail.com>
2025-06-10 18:03:27 +0200

    1.0.0 - Refactor = Début du refactor du code
    Le refactor a pour but de décomposer tous les fichiers utilitaires (d'extention .py et .ipynb sauf 'Fresqu'aeromaps v1.2.ipynb') en une série de fichiers .py.
    
    Ce refactor permettra de construire une architecture claire, permettant ainsi de séparer tous les codes métiers, d'alléger les différents appels entre chaque module, ext...
    
    Toutes ces améliorations vont ainsi rendre le code du projet plus facilement maintenable, modulaire et testable.

--- Setup_des_graphs_v1.py
+++ Setup_des_graphs_v1.py
@@ -1,93 +1,96 @@
-###Fichier qui sera appelé par run_graph_3 et qui sert à avoir des fon,c
+# Fichier qui sera appelé par "run_graph_v3.ipynb" et qui sert à initialiser les graphes.
+
+from bqplot import LinearScale, Lines, ColorScale, OrdinalScale
 
 
-import bqplot as bq
-import numpy as np 
-from bqplot import LinearScale, Lines, Axis, Figure, ColorScale, OrdinalScale, Bars
+
+
 sc_x = LinearScale()
 sc_y = LinearScale()
 sc_col = ColorScale(colors=["Black","Blue", "Yellow","Orange","Green","Magenta","Red"])
 
-##fonction de tracé du graph des émissions annuelles qui dépend de process_data, qui sera calculé dans run_graph_3 en fonciton des widgets
+
+# Fonction de tracé du graphe des émissions annuelles (qui dépend de process_data, qui sera calculé dans "run_graph_v3.ipynb" en fonciton des widgets) :
 def plot_traj(process_data):
-    #séparation des différents types de data
+    # Séparation des différents types de données :
     df = process_data["vector_outputs"]
     df_climate = process_data["climate_outputs"]
     years = process_data["years"]["full_years"]
     historic_years = process_data["years"]["historic_years"]
     prospective_years = process_data["years"]["prospective_years"]
-    
-    ax_x = Axis(scale=sc_x, label="Années")
-    ax_y = Axis(scale=sc_y, orientation="vertical", label="Emissions de CO2, en Mt")
 
     line = Lines(
-        x=years, 
-        y=[df.loc[years,"co2_emissions_2019technology_baseline3"],
-           df["co2_emissions_2019technology"],
-           df["co2_emissions_including_aircraft_efficiency"],
-           df["co2_emissions_including_load_factor"],
-           df["co2_emissions_including_energy"],
-           df_climate.loc[years, "co2_emissions"] - df.loc[years, "carbon_offset"]],
-        color=[1,2,3,4,5,6],
-        stroke_width=0,
-        fill='between',
-        fill_colors=["Blue","Yellow","Orange","Green","Magenta"],
-        fill_opacities=[0.3]*5,
-        labels=["Changement de la demande","Efficacité technologique","Opérations en vol",
-                "Energies alternatives","Compensation carbone","Emission restantes"],
-        display_legend=True,
-        scales={"x": sc_x, "y": sc_y, "color" : sc_col},
+        x = years, 
+        y = [
+            df.loc[years,"co2_emissions_2019technology_baseline3"],
+            df["co2_emissions_2019technology"],
+            df["co2_emissions_including_aircraft_efficiency"],
+            df["co2_emissions_including_load_factor"],
+            df["co2_emissions_including_energy"],
+            df_climate.loc[years, "co2_emissions"] - df.loc[years, "carbon_offset"]
+        ],
+        color = [1,2,3,4,5,6],
+        stroke_width = 0,
+        fill = 'between',
+        fill_colors = ["Blue","Yellow","Orange","Green","Magenta"],
+        fill_opacities = [0.3] * 5,
+        labels = ["Changement de la demande", "Efficacité technologique", "Opérations en vol", "Energies alternatives", "Compensation carbone", "Emission restantes"],
+        display_legend = True,
+        scales = {"x": sc_x, "y": sc_y, "color" : sc_col},
     )
 
     line_h = Lines(
-        x=historic_years, 
-        y=df_climate.loc[historic_years, "co2_emissions"],
-        color=[0], 
-        scales={"x": sc_x, "y": sc_y, "color" : sc_col}
+        x = historic_years, 
+        y = df_climate.loc[historic_years, "co2_emissions"],
+        color = [0], 
+        scales = {"x": sc_x, "y": sc_y, "color" : sc_col}
     )
 
     line_p = Lines(
-        x=prospective_years, 
-        y=[df_climate.loc[prospective_years, "co2_emissions"] - df.loc[prospective_years, "carbon_offset"],
-           df.loc[prospective_years,"co2_emissions_2019technology_baseline3"]],
-        color=[6,0], 
-        scales={"x": sc_x, "y": sc_y, "color" : sc_col},
+        x = prospective_years, 
+        y = [
+            df_climate.loc[prospective_years, "co2_emissions"] - df.loc[prospective_years, "carbon_offset"],
+            df.loc[prospective_years,"co2_emissions_2019technology_baseline3"]
+        ],
+        color = [6,0], 
+        scales = {"x": sc_x, "y": sc_y, "color" : sc_col},
     )
-    
 
-    return [line,line_h,line_p]
-    
-#initialisation des axes pour le graph multi-disciplinaire
+    return [line, line_h, line_p]
+
+
+# Initialisation des axes pour le graphe multi-disciplinaire :
 categories = ["Climat\n(Total)","Climat\n(CO₂)","Biomasse","Electricité"]
 x_ord = OrdinalScale()
 y_sc = LinearScale()
 
-##fonction de tracé du graph multi-disciplinaire 
+
+# Fonction de tracé du graphe multi-disciplinaire :
 def plot_multi(process_data):
     parameters = process_data["float_inputs"]
     df = process_data["vector_outputs"]
     df_climate = process_data["climate_outputs"]
     float_outputs = process_data["float_outputs"]
-    years = process_data["years"]["full_years"]
-    historic_years = process_data["years"]["historic_years"]
-    prospective_years = process_data["years"]["prospective_years"]
+    years = process_data["years"]["full_years"]                    # Non-utilisé pour le moment.
+    historic_years = process_data["years"]["historic_years"]       # Non-utilisé pour le moment.
+    prospective_years = process_data["years"]["prospective_years"] # Non-utilisé pour le moment.
 
-    # Carbon budget
+    # Budget carbone :
     gross_carbon_budget = float(float_outputs["gross_carbon_budget_2050"])
     aviation_carbon_budget = float(float_outputs["aviation_carbon_budget"])
     cumulative_co2_emissions = float(df.loc[2050, "cumulative_co2_emissions"])
 
-    # Biomass
+    # Biomasse :
     available_biomass_total = float_outputs["available_biomass_total"]
     aviation_available_biomass = float_outputs["aviation_available_biomass"]
     biomass_consumption_end_year = float_outputs["biomass_consumption_end_year"]
 
-    # Electricity
+    # Electricité :
     available_electricity_total = parameters["available_electricity"]
     aviation_available_electricity = float_outputs["aviation_available_electricity"]
     electricity_consumption_end_year = float_outputs["electricity_consumption_end_year"]
 
-    # Effective radiative forcing
+    # Forçage radiatif effectif :
     equivalent_gross_carbon_budget = float(float_outputs["equivalent_gross_carbon_budget_2050"])
     aviation_equivalent_carbon_budget = float(float_outputs["aviation_equivalent_carbon_budget"])
     cumulative_total_equivalent_emissions = float(df_climate.loc[2050, "cumulative_total_equivalent_emissions"])
@@ -96,12 +99,14 @@ def plot_multi(process_data):
         aviation_equivalent_carbon_budget / equivalent_gross_carbon_budget * 100,
         aviation_carbon_budget / gross_carbon_budget * 100,
         aviation_available_biomass / available_biomass_total * 100,
-        aviation_available_electricity / available_electricity_total * 100,]
+        aviation_available_electricity / available_electricity_total * 100
+    ]
 
     consumptions = [
-        np.max([cumulative_total_equivalent_emissions / equivalent_gross_carbon_budget * 100, 0]),
+        max(cumulative_total_equivalent_emissions / equivalent_gross_carbon_budget * 100, 0),
         cumulative_co2_emissions / gross_carbon_budget * 100,
         biomass_consumption_end_year / available_biomass_total * 100,
-        electricity_consumption_end_year / available_electricity_total * 100,]
+        electricity_consumption_end_year / available_electricity_total * 100
+    ]
 
-    return [consumptions,budgets]
+    return [consumptions, budgets]
