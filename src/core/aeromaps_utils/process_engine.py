641a772552e9d96cb415394ad8bcc32835791146
tominien <tom.leber.toulouse@gmail.com>
2025-07-04 16:12:59 +0200

    2.2.0 - Suppression des variables répétées (par rapport au scénario de base) lors de l'initialisation des modèles d'AéroMAPS

--- src/core/aeromaps_utils/process_engine.py
+++ src/core/aeromaps_utils/process_engine.py
@@ -42,37 +42,16 @@ def compute_process(
     parameters: Parameters = process.parameters
 
     # Air traffic evolution :
-        # Growth rate by category [%] :
-    parameters.cagr_passenger_short_range_reference_periods         = [2020, 2030, 2040, 2050]
-    parameters.cagr_passenger_short_range_reference_periods_values  = [3.0, 3.0, 3.0]
-    parameters.cagr_passenger_medium_range_reference_periods        = []
-    parameters.cagr_passenger_medium_range_reference_periods_values = [3.0]
-    parameters.cagr_passenger_long_range_reference_periods          = []
-    parameters.cagr_passenger_long_range_reference_periods_values   = [3.0]
-    parameters.cagr_freight_reference_periods                       = []
-    parameters.cagr_freight_reference_periods_values                = [3.0]
-
-    # Aircraft fleet and operation evolution - Aircraft load factor :
-        # Aircraft load factor in 2050 [%] :
-    parameters.load_factor_end_year = 85.0 # 2019 value = 82.399312
+        # Growth rate by category :
+    parameters.cagr_passenger_short_range_reference_periods        = [2020, 2030, 2040, 2050]
+    parameters.cagr_passenger_short_range_reference_periods_values = [3.0, 3.0, 3.0]
 
     # Aircraft fleet and operation evolution - Aircraft efficiency using the top-down approach :
+    parameters.fleet_renewal_duration = 20.0
         # Drop-in aircraft :
-            # Mean annual efficiency gains by category [%] :
-    parameters.energy_per_ask_short_range_dropin_fuel_gain_reference_years         = []
     parameters.energy_per_ask_short_range_dropin_fuel_gain_reference_years_values  = [0.5]
-    parameters.energy_per_ask_medium_range_dropin_fuel_gain_reference_years        = []
     parameters.energy_per_ask_medium_range_dropin_fuel_gain_reference_years_values = [0.5]
-    parameters.energy_per_ask_long_range_dropin_fuel_gain_reference_years          = []
     parameters.energy_per_ask_long_range_dropin_fuel_gain_reference_years_values   = [0.5]
-        # Hydrogen aircraft :
-            # Values for setting logistic functions by category :
-    parameters.hydrogen_final_market_share_short_range = 0.0
-    parameters.hydrogen_introduction_year_short_range  = 2051
-    parameters.fleet_renewal_duration                  = 20.0
-            # Relative energy consumption for hydrogen aircraft with respect to drop-in aircraft [%] :
-    parameters.relative_energy_per_ask_hydrogen_wrt_dropin_short_range_reference_years        = []
-    parameters.relative_energy_per_ask_hydrogen_wrt_dropin_short_range_reference_years_values = [1.0]
 
     # Aircraft fleet and operation evolution - Operations :
         # Values for setting the logistic function :
@@ -81,7 +60,7 @@ def compute_process(
     parameters.operations_duration   = 25.0
 
     # Aircraft energy - Introduction of alternative drop-in fuels :
-        # Share of alternative fuels in the drop-in fuel mix (the rest being supplemented by kerosene) [%] :
+        # Share of alternative fuels in the drop-in fuel mix (the rest being supplemented by kerosene) :
     parameters.biofuel_share_reference_years            = [2020, 2030, 2040, 2050]
     parameters.biofuel_share_reference_years_values     = [0.0, 0.0, 0.0, 0.0]
     parameters.electrofuel_share_reference_years        = [2020, 2030, 2040, 2050]
@@ -94,27 +73,19 @@ def compute_process(
     parameters.residual_carbon_offset_share_reference_years_values           = [0.0, 0.0, 0.0, 0.0]
 
     # Environmental limits :
-        # Carbon budgets and Carbon Dioxide Removal [GtCO2] :
+        # Carbon budgets and Carbon Dioxide Removal :
     parameters.net_carbon_budget           = 850.0
     parameters.carbon_dioxyde_removal_2100 = 280.0
-        # Available energy resources in 2050 [EJ] :
-    parameters.waste_biomass                 = 12.0
-    parameters.crops_biomass                 = 63.0
-    parameters.forest_residues_biomass       = 17.0
-    parameters.agricultural_residues_biomass = 57.0
-    parameters.algae_biomass                 = 15.0
-    parameters.available_electricity         = 250.0
+        # Available energy resources in 2050 :
+    parameters.available_electricity = 250.0
 
     # Allocation settings :
-        # Aviation share of the global (equivalent) carbon budget [%] :
-    parameters.aviation_carbon_budget_allocated_share          = 2.6
-    parameters.aviation_equivalentcarbonbudget_allocated_share = 5.1
-        # Aviation share of the global energy resources (biomass and electricity) [%] :
+        # Aviation share of the global energy resources (biomass and electricity) :
     parameters.aviation_biomass_allocated_share     = 5.0
     parameters.aviation_electricity_allocated_share = 5.0
 
     # Various environmental settings :
-        # Share of biofuel production pathways (the rest being completed by AtJ processes) [%] :
+        # Share of biofuel production pathways (the rest being completed by AtJ processes) :
     parameters.biofuel_hefa_fog_share_reference_years           = [2020, 2030, 2040, 2050]
     parameters.biofuel_hefa_fog_share_reference_years_values    = [100, 100, 0.7, 0.7]
     parameters.biofuel_hefa_others_share_reference_years        = [2020, 2030, 2040, 2050]
@@ -126,7 +97,7 @@ def compute_process(
         # Emission factors for electricity (2019 value: 429 gCO2/kWh) :
     parameters.electricity_emission_factor_reference_years        = [2020, 2030, 2040, 2050]
     parameters.electricity_emission_factor_reference_years_values = [429.0, 200.0, 100.0, 30.0]
-        # Share of hydrogen production pathways (the rest being completed by production via coal without CCS, distribution in 2019: Gas without CCS (71%), Coal without CCS (27%), Electrolysis (2%), Others with CCS (0%), Co-products not taken into account) [%] :
+        # Share of hydrogen production pathways (the rest being completed by production via coal without CCS, distribution in 2019: Gas without CCS (71%), Coal without CCS (27%), Electrolysis (2%), Others with CCS (0%), Co-products not taken into account) :
     parameters.hydrogen_electrolysis_share_reference_years        = [2020, 2030, 2040, 2050]
     parameters.hydrogen_electrolysis_share_reference_years_values = [2, 100, 100, 100]
     parameters.hydrogen_gas_ccs_share_reference_years             = [2020, 2030, 2040, 2050]
@@ -135,30 +106,9 @@ def compute_process(
     parameters.hydrogen_coal_ccs_share_reference_years_values     = [0, 0, 0, 0]
     parameters.hydrogen_gas_share_reference_years                 = [2020, 2030, 2040, 2050]
     parameters.hydrogen_gas_share_reference_years_values          = [71, 0, 0, 0]
-            # Economic aspects :
-    parameters.co2_cost_reference_years                      = []
-    parameters.co2_cost_reference_years_values               = [0.225]
-    parameters.carbon_tax_reference_years                    = []
-    parameters.carbon_tax_reference_years_values             = [5.0]
-    parameters.exogenous_carbon_price_reference_years        = [2020, 2030, 2040, 2050] # Non trouvé.
-    parameters.exogenous_carbon_price_reference_years_values = [54, 250, 500, 775]      # Non trouvé.
-    parameters.carbon_offset_price_reference_years           = []
-    parameters.carbon_offset_price_reference_years_values    = [5.0]
 
     # Apply cards if provided :
     if cards_ids:
-        # Budget carbone :
-        if "carbon_budget" in cards_ids:
-            ...
-    
-        # Réglementations et mesures économiques :
-        if "reglementations_and_economical_measures" in cards_ids:
-            ...
-
-        # Sensibiliser & Éduquer :
-        if "awareness_and_education" in cards_ids:
-            ...
-
         # Sobriété :
         if "sobriety" in cards_ids:
             parameters.cagr_passenger_medium_range_reference_periods_values = [1.5]
@@ -171,8 +121,8 @@ def compute_process(
 
         # Nouveaux vecteurs énergétiques :
         if "new_energies" in cards_ids:
-            parameters.biofuel_share_reference_years_values     = [0.0, 4.8, 24.0, 35.0] # Vérifier ces données !
-            parameters.electrofuel_share_reference_years_values = [0.0, 1.2, 10.0, 35.0] # Vérifier ces données !
+            parameters.biofuel_share_reference_years_values     = [0.0, 4.8, 24.0, 35.0]
+            parameters.electrofuel_share_reference_years_values = [0.0, 1.2, 10.0, 35.0]
 
         # Report modal :
         if "modal_shift" in cards_ids:
@@ -193,8 +143,19 @@ def compute_process(
             parameters.energy_per_ask_short_range_dropin_fuel_gain_reference_years_values  = [1.0]
             parameters.energy_per_ask_medium_range_dropin_fuel_gain_reference_years_values = [1.0]
             parameters.energy_per_ask_long_range_dropin_fuel_gain_reference_years_values   = [1.0]
-            parameters.hydrogen_final_market_share_short_range                             = 0.0
             parameters.fleet_renewal_duration                                              = 15.0
+
+        # Budget carbone :
+        if "carbon_budget" in cards_ids:
+            ...
+    
+        # Réglementations et mesures économiques :
+        if "reglementations_and_economical_measures" in cards_ids:
+            ...
+
+        # Sensibiliser & Éduquer :
+        if "awareness_and_education" in cards_ids:
+            ...
 
     process.compute()
     return process.data
